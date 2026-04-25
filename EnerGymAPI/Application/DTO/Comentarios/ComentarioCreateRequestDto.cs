c74fc62562a3ec9f9c70f7cdb47a5336c90c037c
diego <dieganfer.dev@gmail.com>
2026-04-25 02:47:33 +0200

    Creación de DTOs de Comentario

---
+++ EnerGymAPI/Application/DTO/Comentarios/ComentarioCreateRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnerGymAPI.Application.DTO.Comentarios
+{
+    public class ComentarioCreateRequestDto
+    {
+        public Guid PostId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public required string Contenido { get; set; }
+        public long Fecha { get; set; }
+    }
+}
