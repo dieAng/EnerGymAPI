30ba5aa859b0ca67653d0e2a11b33376d3acde8b
diego <dieganfer.dev@gmail.com>
2026-04-25 03:11:42 +0200

    Creación de LikeController.cs

---
+++ EnerGymAPI/Controllers/LikeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using EnerGymAPI.Application.DTO.Likes;
+using EnerGymAPI.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnerGymAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikeController : ControllerBase
+    {
+        private readonly ILikeService _likeService;
+
+        public LikeController(ILikeService likeService)
+        {
+            _likeService = likeService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ToggleLike(LikeRequestDto request)
+        {
+            var message = await _likeService.ToggleLikeAsync(request);
+            return Ok(new { message });
+        }
+    }
+}
