691d272ec7dbb4c4080ac73b1edf74adf0eb4793
diego <dieganfer.dev@gmail.com>
2026-04-25 02:55:37 +0200

    Creación de IEjercicioService.cs

---
+++ EnerGymAPI/Core/Interfaces/IEjercicioService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EnerGymAPI.Application.DTO.Ejercicios;
+
+namespace EnerGymAPI.Core.Interfaces
+{
+    public interface IEjercicioService
+    {
+        Task<IEnumerable<EjercicioResponseDto>> GetEjerciciosAsync();
+        Task<EjercicioResponseDto> GetEjercicioByIdAsync(Guid id);
+        Task<EjercicioResponseDto> CreateEjercicioAsync(EjercicioCreateRequestDto request);
+    }
+}
