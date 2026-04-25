62878a3365877b4e5493983fe25109cad507c53f
diego <dieganfer.dev@gmail.com>
2026-04-25 03:11:26 +0200

    Creación de EjercicioController.cs

---
+++ EnerGymAPI/Controllers/EjercicioController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EnerGymAPI.Application.DTO.Ejercicios;
+using EnerGymAPI.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnerGymAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EjercicioController : ControllerBase
+    {
+        private readonly IEjercicioService _ejercicioService;
+
+        public EjercicioController(IEjercicioService ejercicioService)
+        {
+            _ejercicioService = ejercicioService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EjercicioResponseDto>>> GetEjercicios()
+        {
+            var ejercicios = await _ejercicioService.GetEjerciciosAsync();
+            return Ok(ejercicios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EjercicioResponseDto>> GetEjercicio(Guid id)
+        {
+            var ejercicio = await _ejercicioService.GetEjercicioByIdAsync(id);
+            if (ejercicio == null)
+            {
+                return NotFound();
+            }
+            return Ok(ejercicio);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public async Task<ActionResult<EjercicioResponseDto>> CreateEjercicio(EjercicioCreateRequestDto request)
+        {
+            var nuevoEjercicio = await _ejercicioService.CreateEjercicioAsync(request);
+            return CreatedAtAction(nameof(GetEjercicio), new { id = nuevoEjercicio.Id }, nuevoEjercicio);
+        }
+    }
+}
