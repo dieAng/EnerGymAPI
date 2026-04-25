9fa2ebe257af47de9413662c9f86aa37235f609f
diego <dieganfer.dev@gmail.com>
2026-04-25 02:55:25 +0200

    Creación de IComentarioService.cs

---
+++ EnerGymAPI/Core/Interfaces/IComentarioService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EnerGymAPI.Application.DTO.Comentarios;
+
+namespace EnerGymAPI.Core.Interfaces
+{
+    public interface IComentarioService
+    {
+        Task<IEnumerable<ComentarioResponseDto>> GetComentariosPorPostAsync(Guid postId);
+        Task<ComentarioResponseDto> CreateComentarioAsync(ComentarioCreateRequestDto request);
+        Task<bool> DeleteComentarioAsync(Guid id);
+    }
+}
