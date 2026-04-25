868a613414d00e034904b26141e506c1bcf9d455
diego <dieganfer.dev@gmail.com>
2026-04-25 03:11:54 +0200

    Creación de PostController.cs

---
+++ EnerGymAPI/Controllers/PostController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EnerGymAPI.Application.DTO.Posts;
+using EnerGymAPI.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnerGymAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PostResponseDto>>> GetPosts()
+        {
+            var posts = await _postService.GetPostsAsync();
+            return Ok(posts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PostResponseDto>> GetPost(Guid id)
+        {
+            var post = await _postService.GetPostByIdAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PostResponseDto>> CreatePost(PostCreateRequestDto request)
+        {
+            var nuevoPost = await _postService.CreatePostAsync(request);
+            return CreatedAtAction(nameof(GetPost), new { id = nuevoPost.Id }, nuevoPost);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePost(Guid id)
+        {
+            var resultado = await _postService.DeletePostAsync(id);
+            if (!resultado)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
