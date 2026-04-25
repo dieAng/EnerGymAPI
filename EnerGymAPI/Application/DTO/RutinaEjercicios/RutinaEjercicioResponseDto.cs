5bcd42019d53960ccf0ac243e94ffe79fc23388b
diego <dieganfer.dev@gmail.com>
2026-04-25 02:50:04 +0200

    Creación de DTOs de RutinaEjercicio

---
+++ EnerGymAPI/Application/DTO/RutinaEjercicios/RutinaEjercicioResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnerGymAPI.Application.DTO.RutinaEjercicios
+{
+    public class RutinaEjercicioResponseDto
+    {
+        public Guid RutinaId { get; set; }
+        public Guid EjercicioId { get; set; }
+        public int Series { get; set; }
+        public int Repeticiones { get; set; }
+        public float? PesoObjetivo { get; set; }
+        public int? DescansoSeg { get; set; }
+        public int Orden { get; set; }
+    }
+}
