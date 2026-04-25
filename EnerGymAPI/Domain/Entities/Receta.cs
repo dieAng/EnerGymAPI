4f432ce3f4ed1866e06260403424507b44250960
diego <dieganfer.dev@gmail.com>
2026-04-25 02:33:31 +0200

    Creación del modelo Receta.cs

---
+++ EnerGymAPI/Domain/Entities/Receta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+ namespace EnerGymAPI.Domain.Entities
+{
+    public class Receta
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UsuarioId { get; set; }
+        public required string Nombre { get; set; }
+        public int? TiempoPreparacion { get; set; }
+        public string? Alergenos { get; set; }
+        public string? ImagenUrl { get; set; }
+        public string? Descripcion { get; set; }
+        public bool EsPredisenada { get; set; } = false;
+
+         // Relaciones
+        public Usuario? Usuario { get; set; }
+    }
+}
