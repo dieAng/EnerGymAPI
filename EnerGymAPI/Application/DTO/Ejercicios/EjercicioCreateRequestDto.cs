6109240902c41e050a262caa8c30b785d776f0f1
diego <dieganfer.dev@gmail.com>
2026-04-25 02:47:49 +0200

    Creación de DTOs de Ejercicio

---
+++ EnerGymAPI/Application/DTO/Ejercicios/EjercicioCreateRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EnerGymAPI.Application.DTO.Ejercicios
+{
+    public class EjercicioCreateRequestDto
+    {
+        public required string Nombre { get; set; }
+        public string? GrupoMuscular { get; set; }
+        public string? Equipo { get; set; }
+        public string? Descripcion { get; set; }
+        public string? ImagenUrl { get; set; }
+        public string? VideoUrl { get; set; }
+    }
+}
---
+++ EnerGymAPI/Application/DTO/Ejercicios/EjercicioResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnerGymAPI.Application.DTO.Ejercicios
+{
+    public class EjercicioResponseDto
+    {
+        public Guid Id { get; set; }
+        public required string Nombre { get; set; }
+        public string? GrupoMuscular { get; set; }
+        public string? Equipo { get; set; }
+        public string? Descripcion { get; set; }
+        public string? ImagenUrl { get; set; }
+        public string? VideoUrl { get; set; }
+    }
+}
