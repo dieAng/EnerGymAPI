66887895f368dab381b6c0326587b0f1b50484a4
diego <dieganfer.dev@gmail.com>
2026-04-25 02:56:34 +0200

    Creación de IRecetaService.cs

---
+++ EnerGymAPI/Core/Interfaces/IRecetaService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EnerGymAPI.Application.DTO.Recetas;
+
+namespace EnerGymAPI.Core.Interfaces
+{
+    public interface IRecetaService
+    {
+        Task<IEnumerable<RecetaResponseDto>> GetRecetasAsync();
+        Task<RecetaResponseDto> GetRecetaByIdAsync(Guid id);
+        Task<RecetaResponseDto> CreateRecetaAsync(RecetaCreateRequestDto request);
+        Task<bool> DeleteRecetaAsync(Guid id);
+    }
+}
