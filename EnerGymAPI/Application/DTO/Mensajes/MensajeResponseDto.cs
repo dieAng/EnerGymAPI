4bcf7bef2c133cb5d1a1fb1bb5aa928e83f3af71
diego <dieganfer.dev@gmail.com>
2026-04-25 02:48:59 +0200

    Creación de DTOs de Mensaje

---
+++ EnerGymAPI/Application/DTO/Mensajes/MensajeResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EnerGymAPI.Application.DTO.Mensajes
+{
+    public class MensajeResponseDto
+    {
+        public Guid Id { get; set; }
+        public Guid EmisorId { get; set; }
+        public Guid ReceptorId { get; set; }
+        public required string Contenido { get; set; }
+        public long Fecha { get; set; }
+    }
+}
