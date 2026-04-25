cf3ab9100cc925ee63f29b795f90f46567c8b39e
diego <dieganfer.dev@gmail.com>
2026-04-25 02:32:20 +0200

    Creación del modelo Ingrediente.cs

---
+++ EnerGymAPI/Domain/Entities/Ingrediente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+ namespace EnerGymAPI.Domain.Entities
+{
+    public class Ingrediente
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Nombre { get; set; }
+        public float? Calorias { get; set; }
+        public float? Proteinas { get; set; }
+        public float? Carbohidratos { get; set; }
+        public float? Grasas { get; set; }
+    }
+}
