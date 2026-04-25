46588723e2bf2c42b5950747f91f40066ded551c
diego <dieganfer.dev@gmail.com>
2026-04-25 02:49:43 +0200

    Creación de DTOs de Rutina

---
+++ EnerGymAPI/Application/DTO/Rutinas/RutinaCreateRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnerGymAPI.Application.DTO.Rutinas
+{
+    public class RutinaCreateRequestDto
+    {
+        public Guid UsuarioId { get; set; }
+        public required string Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Nivel { get; set; }
+        public string? Objetivo { get; set; }
+        public bool EsPredisenada { get; set; }
+    }
+}
---
+++ EnerGymAPI/Application/DTO/Rutinas/RutinaResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnerGymAPI.Application.DTO.Rutinas
+{
+    public class RutinaResponseDto
+    {
+        public Guid Id { get; set; }
+        public Guid UsuarioId { get; set; }
+        public required string Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Nivel { get; set; }
+        public string? Objetivo { get; set; }
+        public bool EsPredisenada { get; set; }
+    }
+}
