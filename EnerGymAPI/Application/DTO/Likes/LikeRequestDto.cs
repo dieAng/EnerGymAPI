bdbfde98a9138e84254de8c4d74da00d7be99245
diego <dieganfer.dev@gmail.com>
2026-04-25 02:48:39 +0200

    Creación de DTO de Like

---
+++ EnerGymAPI/Application/DTO/Likes/LikeRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EnerGymAPI.Application.DTO.Likes
+{
+    public class LikeRequestDto
+    {
+        public Guid PostId { get; set; }
+        public Guid UsuarioId { get; set; }
+    }
+}
