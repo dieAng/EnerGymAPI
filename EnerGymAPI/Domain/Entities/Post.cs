22303f322a26d249abee9b8c996fdde8464c57b7
diego <dieganfer.dev@gmail.com>
2026-04-25 02:33:22 +0200

    Creación del modelo Post.cs

---
+++ EnerGymAPI/Domain/Entities/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+ namespace EnerGymAPI.Domain.Entities
+{
+    public class Post
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UsuarioId { get; set; }
+        public string? Contenido { get; set; }
+        public string? ImagenUrl { get; set; }
+        public double EnergiaGenerada { get; set; } = 0.0;
+        public long Fecha { get; set; } // O DateTime, dependiendo de cómo manejes las fechas
+         
+        // Relaciones
+        public Usuario? Usuario { get; set; }
+        public ICollection<LikePost> Likes { get; set; } = new List<LikePost>();
+        public ICollection<Comentario> Comentarios { get; set; } = new List<Comentario>();
+    }
+}
