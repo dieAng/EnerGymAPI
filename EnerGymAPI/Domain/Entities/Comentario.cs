92bea997ca9ff3bff2ab15305bd9936fa99be68f
diego <dieganfer.dev@gmail.com>
2026-04-25 02:30:45 +0200

    Creación del modelo Comentario.cs

---
+++ EnerGymAPI/Domain/Entities/Comentario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+ namespace EnerGymAPI.Domain.Entities
+{
+    public class Comentario
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid PostId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public required string Contenido { get; set; }
+        public long Fecha { get; set; }
+
+         // Relaciones
+        public Post? Post { get; set; }
+        public Usuario? Usuario { get; set; }
+    }
+}
