fc30b37764a2bb71be23f3a8992e2631741cda10
diego <dieganfer.dev@gmail.com>
2026-04-25 02:47:14 +0200

    Creación de DTOs de Auth

---
+++ EnerGymAPI/Application/DTO/Auth/LoginResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EnerGymAPI.Application.DTO.Auth
+{
+    public class LoginResponseDto
+    {
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
+        public Guid UsuarioId { get; set; }
+        public required string Nombre { get; set; }
+        public required string Rol { get; set; }
+    }
+}
---
+++ EnerGymAPI/Application/DTO/Auth/RefreshTokenRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnerGymAPI.Application.DTO.Auth
+{
+    public class RefreshTokenRequestDto
+    {
+        [Required]
+        public required string RefreshToken { get; set; }
+    }
+}
