83a82f28e59c503a8b9aa8f867f19014a9a8f715
diego <dieganfer.dev@gmail.com>
2026-04-25 02:33:44 +0200

    Creación del modelo Rutina.cs

---
+++ EnerGymAPI/Domain/Entities/Rutina.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnerGymAPI.Domain.Entities
+{
+    public class Rutina
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UsuarioId { get; set; }
+        public required string Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Nivel { get; set; }
+        public string? Objetivo { get; set; }
+        public bool EsPredisenada { get; set; } = false;
+
+         // Relaciones
+        public Usuario? Usuario { get; set; }
+        public ICollection<RutinaEjercicio> RutinasEjercicios { get; set; } = new List<RutinaEjercicio>();
+        public ICollection<SesionEntrenamiento> Sesiones { get; set; } = new List<SesionEntrenamiento>();
+    }
+}
